90aa9704a7263701f476ea83dc52803c7760618f
Ben Allfree <ben@benallfree.com>
2012-08-17 07:37:14 -0700

    Added eval_haml() listener

--- load.php
+++ load.php
@@ -1,3 +1,10 @@
 <?
 
 W::add_mixin('HamlMixin');
+
+W::register_filter('eval_haml', function($php, $vars) {
+  $fname = tempnam(sys_get_temp_dir(), "wicked_eval_haml");
+  file_put_contents($fname, $php);
+  $s = W::haml_eval_file($fname, $vars, true);
+  return $s;
+});
